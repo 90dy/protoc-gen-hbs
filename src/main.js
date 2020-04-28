e33ff0c8566e64af09b8475f8dac36e89ef3b91f
Godefroy Ponsinet <godefroy.ponsinet@outlook.com>
2020-04-28 15:16:49 +0200

    chore(main): done
    Signed-off-by: Godefroy Ponsinet <godefroy.ponsinet@outlook.com>

--- src/main.js
+++ src/main.js
@@ -1,49 +1,70 @@
 #!/usr/bin/env node
 
-const {ExportMap} = require('ts-protoc-gen/lib/ExportMap')
 const {CodeGeneratorRequest, CodeGeneratorResponse} = require('google-protobuf/google/protobuf/compiler/plugin_pb')
-const {FileDescriptorProto} = require('google-protobuf/google/protobuf/descriptor_pb')
-const {replaceProtoSuffix, withAllStdIn} = require('ts-protoc-gen/lib/util')
 const handlebars = require('handlebars')
-const helpers = require('handlebars-helpers')
+const dree = require('dree')
+const fs = require('fs')
 
-withAllStdIn((inputBufff: Buffer) => {
-	try {
-		const typedInputBuff = new Uint8Array(inputBuff.length)
-		typedInputBuff.set(inputBuff)
+const handlebarsOptions = {
+	// data: false,
+	// compat: true,
+	// knownHelpers: '',
+	// knownHelpersOnly: true,
+	noEscape: true,
+	// strict: false,
+	// assumeObjects: true,
+	// ignoreStandalone: true,
+	// explicitPartialContext: true,
+}
 
-		const codeGenRequest = CodeGeneratorRequest.deserializeBinary(typedInputBuff)
-		const codeGenResponse = new CodeGeneratorResponse()
-		const exportMap = new ExportMap()
-		const fileNameToDescriptor = {}
+const handlebarsHelpers = [
+	require('./import'),
+	// require('./package'),
+	// require('./message'),
+	// require('./scalar'),
+	// require('./enum'),
+	// require('./service'),
+	// require('./rpc'),
+]
 
-		const parameter = codeGenRequest.getParameter()
+handlebarsHelpers.forEach(helper => helper.register(handlebars))
 
-		codeGenRequest.getProtoFileList().forEach(protoFileDescriptor => {
-			fileNameToDescriptor[protoFileDescriptor.getName()] = protoFileDescriptor
-			exportMap.addFileDescriptor(protoFileDescriptor)
+const inputBuff = fs.readFileSync(0, 'utf-8')
+
+try {
+	const typedInputBuff = new Uint8Array(inputBuff.length)
+	typedInputBuff.set(inputBuff)
+
+	const codeGenRequest = CodeGeneratorRequest.deserializeBinary(typedInputBuff)
+	const codeGenResponse = new CodeGeneratorResponse()
+
+	const responseFileMap = {}
+	dree.scan(codeGenRequest.getParameter() || process.cwd(), { extensions: ['hbs'] }, template => {
+		codeGenRequest.getFileToGenerateList().forEach(file => {
+			let name = ''
+			try {
+				name = handlebars.compile(template.name.replace(/\.hbs$/, ''), handlebarsOptions)(file)
+			} catch (err) {
+				throw new Error(err.message + 'for template ' + template.name + ' when generating file name')
+			}
+			responseFileMap[name] = responseFileMap[name] || ''
+			try {
+				responseFileMap[name] += handlebars.compile(fs.readFileSync(template.path, 'utf8'), handlebarsOptions)(file)
+			} catch (err) {
+				throw new Error(err.message + ' for template ' + template.name + ' line ' + err.lineNumber)
+			}
 		})
+	})
 
-		codeGenRequest.getFileToGenerateList().forEach(fileName => {
-			const basename = replaceProtoSuffix(fileName)
-			const sqlFile = new CodeGeneratorResponse.File();
-			sqlFile.setName(basename + ".sql");
-			sqlFile.setContent(printFileDescriptor(fileNameToDescriptor[fileName], exportMap, 'sql'));
-			codeGenResponse.addFile(sqlFile);
-		})
+	Object.entries(responseFileMap).forEach(([name, content]) => {
+		const file = new CodeGeneratorResponse.File()
+		file.setName(name)
+		file.setContent(content)
+		codeGenResponse.addFile(file)
+	})
 
-		const fileToGenerateList = codeGenRequest.getFileToGenerateList().map(fileName => {
-			// TODO: generate custom filename via hbs
-		})
-
-		// TODO: for each file merge file descriptor
-
-		// TODO: for each file compile handlebars with helpers
-
-		process.stdout.write(Buffer.from(codeGenResponse.serializeBinary()))
-	} catch (error) {
-		console.error("protoc-gen-hbs error: " + err.stack + "\n")
-		process.exit(1)
-	}
-})
-
+	process.stdout.write(Buffer.from(codeGenResponse.serializeBinary()))
+} catch (err) {
+	console.error("protoc-gen-hbs error: " + err.stack + "\n")
+	process.exit(1)
+}
