deb8c035c1c27984eb358eba4461ab16940b995d
Godefroy Ponsinet <godefroy.ponsinet@outlook.com>
2020-04-28 15:16:49 +0200

    chore(main): done
    Signed-off-by: Godefroy Ponsinet <godefroy.ponsinet@outlook.com>

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 
 const {CodeGeneratorRequest, CodeGeneratorResponse} = require('google-protobuf/google/protobuf/compiler/plugin_pb')
 const handlebars = require('handlebars')
+const handlebarsHelpers = require('handlebars-helpers')
 const dree = require('dree')
 const fs = require('fs')
 
@@ -17,8 +18,9 @@ const handlebarsOptions = {
 	// explicitPartialContext: true,
 }
 
-const handlebarsHelpers = [
+const handlebarsProtobufHelpers = [
 	require('./import'),
+	// require('./file'),
 	// require('./package'),
 	// require('./message'),
 	// require('./scalar'),
@@ -27,43 +29,53 @@ const handlebarsHelpers = [
 	// require('./rpc'),
 ]
 
-handlebarsHelpers.forEach(helper => helper.register(handlebars))
+handlebarsHelpers({ handlebars })
+handlebarsProtobufHelpers.forEach(helper => helper.register(handlebars))
 
-const inputBuff = fs.readFileSync(0, 'utf-8')
+const requestBuffer = fs.readFileSync(0, 'utf-8')
 
 try {
-	const typedInputBuff = new Uint8Array(inputBuff.length)
-	typedInputBuff.set(inputBuff)
+	const requestUint8Array = new Uint8Array(requestBuffer.length)
+	requestUint8Array.set(requestBuffer)
 
-	const codeGenRequest = CodeGeneratorRequest.deserializeBinary(typedInputBuff)
-	const codeGenResponse = new CodeGeneratorResponse()
+	const request = CodeGeneratorRequest.deserializeBinary(requestUint8Array)
+	const response = new CodeGeneratorResponse()
 
-	const responseFileMap = {}
-	dree.scan(codeGenRequest.getParameter() || process.cwd(), { extensions: ['hbs'] }, template => {
-		codeGenRequest.getFileToGenerateList().forEach(file => {
-			let name = ''
+	const fileDescriptorMap = request.getFileToGenerateList().reduce((map, name) => {
+		map[name] = request.getProtoFileList().find(_ => _.getName() === name)
+		return map
+	}, {})
+
+	const templateRequestMap = {}
+
+	dree.scan(request.getParameter() || process.cwd(), { extensions: ['hbs'] }, template => {
+		Object.values(fileDescriptorMap).forEach((fileDescriptor) => {
+			let templateRequestName = ''
 			try {
-				name = handlebars.compile(template.name.replace(/\.hbs$/, ''), handlebarsOptions)(file)
+				templateRequestName = handlebars.compile(decodeURIComponent(template.name.replace(/\.hbs$/, '')), handlebarsOptions)(fileDescriptor)
 			} catch (err) {
 				throw new Error(err.message + 'for template ' + template.name + ' when generating file name')
 			}
-			responseFileMap[name] = responseFileMap[name] || ''
-			try {
-				responseFileMap[name] += handlebars.compile(fs.readFileSync(template.path, 'utf8'), handlebarsOptions)(file)
-			} catch (err) {
-				throw new Error(err.message + ' for template ' + template.name + ' line ' + err.lineNumber)
+			if (templateCodeGenRequest[templateRequestName]) {
+				templateRequestMap[templateRequestName].addFileToGenerate(name)
+			} else {
+				templateRequestMap[templateRequestName] = new CodeGeneratorRequest({ ...request, fileToGenerateList: [fileDescriptor.name] })
 			}
 		})
 	})
 
-	Object.entries(responseFileMap).forEach(([name, content]) => {
-		const file = new CodeGeneratorResponse.File()
-		file.setName(name)
-		file.setContent(content)
-		codeGenResponse.addFile(file)
+	Object.entries(templateRequestMap).entries(([name, templateRequest]) => {
+		try {
+			const file = new CodeGeneratorResponse.File()
+			file.setName(name)
+			file.setContent(handlebars.compile(fs.readFileSync(template.path, 'utf8'), handlebarsOptions)(templateRequest))
+			response.addFile(file)
+		} catch (err) {
+			throw new Error(err.message + ' for template ' + template.name + ' line ' + err.lineNumber)
+		}
 	})
 
-	process.stdout.write(Buffer.from(codeGenResponse.serializeBinary()))
+	process.stdout.write(Buffer.from(response.serializeBinary()))
 } catch (err) {
 	console.error("protoc-gen-hbs error: " + err.stack + "\n")
 	process.exit(1)
